0896c5b00cfec0cff4c9552f120e104ed6d83a31
Patryk <hiddenmanpl@gmail.com>
2020-05-18 14:16:36 +0200

    Initial commit for Sick Leave project

---
+++ WebDoctor/Controllers/PeopleController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebDoctor.Models;
+
+namespace WebDoctor.Controllers
+{
+    public class PeopleController : Controller
+    {
+
+        // GET: People
+        public ActionResult Index()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+
+            people.Add(new PersonModel { FirstName = "Patryk", LastName = "Gołębiowski", PESEL = 97082606295, BirthDay = new DateTime(1997, 08, 26).ToShortDateString() });
+            people.Add(new PersonModel { FirstName = "Ania", LastName = "Ciepał", PESEL = 96092309789, BirthDay = new DateTime(1996, 09, 23).ToShortDateString() });
+
+            return View(people);
+        }
+
+        // GET: People/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: People/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: People/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(PersonModel person)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: People/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: People/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: People/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: People/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ WebDoctor/Models/PersonModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace WebDoctor.Models
+{
+    public class PersonModel
+    {
+        [Display(Name = "Imię")]
+        [StringLength(12, MinimumLength = 3, ErrorMessage = "Musisz podać poprawne imię")]
+        [Required(AllowEmptyStrings = false)]
+        public string FirstName { get; set; }
+        
+        [Display(Name = "Nazwisko")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Musisz podać poprawne nazwisko")]
+        [Required(AllowEmptyStrings = false)]
+        public string LastName { get; set; }
+       
+        [Display(Name = "Data urodzenia")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "Musisz podać poprawną datę urodzenia")]
+        [DataType(DataType.Date)]
+        [Required(AllowEmptyStrings = false)]
+        public string BirthDay { get; set; }
+
+        [Display(Name = "PESEL")]
+        [StringLength(11, MinimumLength = 11)]
+        [Range(00000000001, 99999999999, ErrorMessage = "Musisz podać poprawny PESEL")]
+        public long PESEL { get; set; }
+    }
+}
